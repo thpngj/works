54684e933eac0c34835a3f8841f0da46bcc3769d
苹果蛋儿 <65292975@qq.com>
2024-10-07 17:54:27 +0800

    添加技术栈说明

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,17 @@
                     <div><img class="w-full inline-block" src="../assets/project-img/ys-01.png"/></div>
                     <div><img class="w-full inline-block" src="../assets/project-img/ys-02.png"/></div>
                   </div>
+                  
                   <div class="p-4 pt-0 lg:text-lg">
-                    <h3 class="font-bold">· 会员可视化 ·</h3><p>实时掌握会员消费情况，随时随地了解会员信息 </p>
-                    <h3 class="font-bold">· 实时利润，实时掌控 ·</h3><p>以利润的核心，数字化可视化为工具，随时掌控经营方向</p>
-                    <h3 class="font-bold">· 货品智能管理 ·</h3><p>助力门店，加速货品流转，高速高效提升货品价值</p>
+                    <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                      <p><span class="font-bold text-blue-500">技术栈：</span>vue2、vue-cli、vuex、axios、vant、antv/f2</p>
+                      <p><span class="font-bold text-green-500">运行平台：</span>微信公众号、微信小程序</p>
+                    </div>
+                    <p>会员可视化:实时掌握会员消费情况，随时随地了解会员信息 </p>
+                    <p>实时利润，实时掌控:以利润的核心，数字化可视化为工具，随时掌控经营方向</p>
+                    <p>货品智能管理:助力门店，加速货品流转，高速高效提升货品价值</p>
                   </div>
+                  
               </div>
         
               <div class="border border-solid dark:border-gray-700 rounded-2xl overflow-hidden">
@@ -29,6 +35,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/nc-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>vue2、axios、vant、weixin-js-sdk</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信公众号</p>
+                  </div>
                   多地区农场租地种植，畜牧领养，果树认领，平台商城、个人跳骚市场、农场云直播监控为一体的客户端平台，解决城市人体验农村养殖、种植生活。
                 </div>
               </div>
@@ -39,6 +49,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/gzm-01.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>webpack、vue2、axios、element-ui、echarts</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>web</p>
+                  </div>
                   对全区物联专网业务、物联网接入管理（网络接入、网络应用情况等）、物联专网终端设备管理（设备台帐、底座台账、传输互连标准化等）、设备可信共享等相关物联网业务研究顶层设计和实施规划方案并统筹建设区级物联网接入和综合监管；建设全区物联感知体系：感知终端、感知载体、感知网络、感知数据共享。
                 </div>
               </div>
@@ -49,6 +63,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/xmxf-01.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>html、css、js、amap(高德)</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>web</p>
+                  </div>
                   着火单位一键报警，消防单位毫秒级响应；对失火单位视频资源查看，制定初步救援方案；规划最优路线，指挥中心对车辆行驶状态、失火现场和水源信息实时查看；对现场救援过程全程视频可视化，实现远程精确作战指挥。
                 </div>
               </div>
@@ -60,6 +78,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/yg-04.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>uniapp、uview-ui</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信小程序</p>
+                  </div>
                   通过该平台可对医院所有资产设备进行管理，在线报修，智能派工，设备维护、智能巡检、医院资产管理等。
                 </div>
               </div>
@@ -71,6 +93,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/cg-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>uniapp、uview-ui</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信公众号、android、ios</p>
+                  </div>
                   北京市的城市管理服务应用，用户可通过市民城管通举报违法违章现象、门店市场导航地图、商家绑定门店时实查看门店违规，商户信用、商户自纠自查，查看周边公共设施、了解法律法规等等。
                 </div>
               </div>
@@ -81,6 +107,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/hdps-01.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>webpack、vue2、axios、element-ui、echarts</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>web</p>
+                  </div>
                   (污水体治理示范城市排水设施信息管理入河排口监管系统)<br/>
                   系统诊断排水系统存在的混接、错接及功能情、结构性缺失等问题，确定建成区所有市政道路雨污水系统混拉错接点追本溯源，确定污染物来源。查明沿河排放口的排水属性及排水量，为系统改善城区水体黑臭情况，改善城区排水条件，满足示范城市建设要求提供基础支撑。
                 </div>
@@ -92,6 +122,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/hdwf-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>vite、vue3、pinia、ant-design-vue、axios、antv/g2plot</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>web</p>
+                  </div>
                   通过与河北省固体废物动态信息平台的数据共享交换，实现危险废物全流程监管数据的共享采集，通过系统与相关单位的人机交互，为邯郸市危废智能监管平台提供信息化交互支撑。
                 </div>
               </div>
@@ -103,6 +137,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/scyf-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>小程序原生开发、vant/weapp</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信小程序</p>
+                  </div>
                   帮助企业建立和完善双重预防机制，检验安全标准统一；安全管理员发现安全隐患后，在系统中录入安全隐患的详细情况，做出整改；整改人收到安全隐患信息后进行整改，整改完成后拍照上传，完成安全隐患整改闭环工作；风险点定期巡查记录，隐患台账一键导出，大大节省了企业的人力物力。
                 </div>
               </div>
@@ -114,6 +152,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/cysz-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>vite、vue3、axios、vant、weixin-js-sdk、tailwindcss</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信公众号</p>
+                  </div>
                   平台提供学生，老师信息维护，学生考核管理，考核统计，学生请假管理，统计报表等功能，移动端一键操作方便老师日常工作效率。
                 </div>
               </div>
@@ -125,6 +167,10 @@
                   <div class="col-span-3"><img class="w-full inline-block" src="../assets/project-img/ixm-02.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>uniapp、uview-ui</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>微信公众号、android、ios</p>
+                  </div>
                   新密政务服务（ i新密）是新密市大数据管理局承办、涵盖政务服务、生活服务、交通出行、政民互动、社会治理、等领域，老百姓通过微信公众号、APP、24小时一体机等进行各项事务办理是新密数字政府、智慧城市、智慧社会建设成果展示、应用的移动端数字政府。
                 </div>
               </div>
@@ -135,6 +181,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/tyj-01.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class="font-bold text-blue-500">技术栈：</span>html、css、js、echarts</p>
+                    <p><span class="font-bold text-green-500">运行平台：</span>web</p>
+                  </div>
                   体育产业统计调查系统是通过体育产品专项调查平台完成体育产业相关法人单位、产业活动单位和个体经营户的调查和分析工作，借助调查系统的手机端、电脑端，一方面实现对调查员和被调查对象的线上管理，另外一方面实现调查数据从现场采集、上报到审核、复核、汇总分析的全流程数据化管理，从而最大程度上节省调查工作的人力物力，提高数据质量和工程效率。
                 </div>
               </div>
@@ -149,6 +199,10 @@
                   <div><img class="w-full inline-block" src="../assets/project-img/htdp-05.png"/></div>
                 </div>
                 <div class="p-4 pt-0 lg:text-lg">
+                  <div class="bg-gray-100 rounded-sm p-3 dark:bg-gray-800">
+                    <p><span class=" text-blue-500">技术栈：</span>vite、vue3、axios、vueuse、echarts、gsap、tailwindcss</p>
+                    <p><span class=" text-green-500">运行平台：</span>web大屏</p>
+                  </div>
                   对于企业而言，每一步操作都可能会影响到最终的收入绩效。而随着数据的日渐积累，管理并从中提取有价值的信息变得越来越难，通过数据大屏简化数据分析，并帮助企业依据数据更科学、更智能、更快更好的制定决策。
                 </div>
               </div>
